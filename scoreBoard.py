830aabbad82a9d4c5cacee1ee455e3086f598c4b
steefaan1993 <45168135+steefaan1993@users.noreply.github.com>
2018-11-26 15:32:17 +0100

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QDockWidget, QLabel
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QDockWidget, QGridLayout,QLabel
 from board import Board
 import sys
+
 
 class ScoreBoard(QDockWidget):
 
@@ -11,13 +11,12 @@ class ScoreBoard(QDockWidget):
 
     def initUI(self):
         '''initiates ScoreBoard UI'''
-        self.lab = QLabel("Player 1")
         self.resize(200, 200)
         self.center()
         self.setWindowTitle('ScoreBoard')
-        self.
+        self.label= QLabel('Player 1')
+        self.setWidget(self.label)
         self.show()
 
     def center(self):
         '''centers the window on the screen'''
-
